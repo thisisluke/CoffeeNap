49c1ed0b7d0b879bcabb7b1c7ffd942d42271baa
lukeliu <luke.liu.au@outlook.com>
2024-05-18 21:11:31 +1000

    Remove storyboard

--- CoffeeNap/HomeViewController.swift
+++ CoffeeNap/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CoffeeNap
 //
 //  Created by Luke Liu on 18/5/2024.
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
