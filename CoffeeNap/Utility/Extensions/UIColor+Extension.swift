703b651b5c3ca0b080b356146a995b88c7d77abb
lukeliu <luke.liu.au@outlook.com>
2024-05-21 22:25:45 +1000

    Add HomeCollectionViewCell and UIColor Extension

---
+++ CoffeeNap/Utility/Extensions/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extension.swift
+//  CoffeeNap
+//
+//  Created by Luke Liu on 21/5/2024.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    b = CGFloat(hexNumber & 0x0000ff) / 255
+                    a = 1.0
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
---
+++ CoffeeNap/View/Screens/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  HomeCollectionViewCell.swift
+//  CoffeeNap
+//
+//  Created by Luke Liu on 21/5/2024.
+//
+
+import UIKit
+import SnapKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    static let identifier = "HomeCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(iconBackgroundView)
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(seperator)
+        
+        iconBackgroundView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(30)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.center.equalTo(iconBackgroundView)
+            make.width.height.equalTo(20)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(iconBackgroundView.snp.trailing).offset(8)
+            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalToSuperview().offset(10)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel.snp.leading)
+            make.trailing.equalTo(titleLabel.snp.trailing)
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.bottom.equalToSuperview().offset(-10)
+        }
+        
+        seperator.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel.snp.leading)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(0.5)
+        }
+    }
+    
+    func configure(with item: HomeItem) {
+        iconImageView.image = UIImage(systemName: item.icon)
+        titleLabel.text = item.title
+        subtitleLabel.text = item.subtitle
+        iconBackgroundView.backgroundColor = UIColor(hex: item.color)
+    }
+    
+    // MARK: - Subviews
+    
+    private let iconBackgroundView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 8
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .label
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let seperator: UIView = {
+        let view = UIView()
+        view.backgroundColor = .separator
+        return view
+    }()
+}
